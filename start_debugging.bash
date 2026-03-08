16eaa7460400f20e22c5f84a87509f74debb35b5
Danny Y. Huang <crazyideas21@gmail.com>
2026-03-08 01:28:39 -0800

    Add additional MAC address to targeted MAC address list in debugging script

--- start_debugging.bash
+++ start_debugging.bash
@@ -15,5 +15,6 @@ touch "$BASE_DIR/control/is_configuration_mode.txt"
 
 # Add sample MAC addresses
 echo "16:30:2a:a2:75:b1" > "$BASE_DIR/control/targeted_mac_address_list.txt"
+echo "1a:08:90:2f:51:e1" >> "$BASE_DIR/control/targeted_mac_address_list.txt"
 
 uv run main.py
