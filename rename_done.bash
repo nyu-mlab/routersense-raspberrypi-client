3063d9c30de874c6e441954f44a4d26f8109b942
Danny Y. Huang <crazyideas21@gmail.com>
2026-03-15 08:02:12 -0700

    Refactor rename_done.bash to dynamically set file extension based on input filename

--- rename_done.bash
+++ rename_done.bash
@@ -4,5 +4,14 @@ new="$1"
 old="$2"
 
 if [ -n "$old" ] && [ "$old" != "$new" ]; then
-    mv "$old" "${old%.tmp}.json"
+    filename="$(basename "$old")"
+    target_base="${old%.tmp}"
+
+    # Default output is .json; packet files are written as .csv.
+    extension=".json"
+    if [[ "$filename" == packets* ]]; then
+        extension=".csv"
+    fi
+
+    mv "$old" "${target_base}${extension}"
 fi
